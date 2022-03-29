4c326e89d5e736aeebb98173eea70ee88e3e0d28
lar4ik69 <r.iking@mail.ru>
2022-03-29 07:18:11 -0700

    2lb1

--- venv/lab2.py
+++ venv/lab2.py
@@ -3,12 +3,16 @@ s_city = "Moscow,RU"
 city_id = 0
 appid = "a168022fb51dd4bc2f1c60c27c5fc556"
 try:
-    res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                        params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
     data = res.json()
-    print("Прогноз погоды на неделю:")
-    for i in data['list']:
-        print( i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'], i['visibility'],  i['wind']['speed'])
+    print("Город:", s_city)
+    print("Погодные условия:", data['weather'][0]['description'])
+    print("Температура:", data['main']['temp'])
+    print("Минимальная температура:", data['main']['temp_min'])
+    print("Максимальная температура", data['main']['temp_max'])
+    print("Видимость:", data['visibility'])
+    print("Скорость ветра:", data['wind']['speed'])
 except Exception as e:
     print("Exception (forecast):", e)
     pass
